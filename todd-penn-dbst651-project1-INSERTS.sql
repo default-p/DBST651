478a5f1ab737f35097f8d191b2caf01791d3fe12
tpenn1 <tpenn1@AUV-UMGCVDA-34.daas.umuc.edu>
2019-12-16 07:52:33 -0500

    Add batch 1 users

--- todd-penn-dbst651-project1-INSERTS.sql
+++ todd-penn-dbst651-project1-INSERTS.sql
@@ -36,9 +36,9 @@ BEGIN
   INSERT INTO todo_comment (body, user_id, todo_id)
   VALUES ('May the force be with you', l_todo_user_id, l_todo_id);
 
-  -- 1. Data for Darth Vader
+  -- 2. Data for Darth Vader
   INSERT INTO todo_user (name, email)
-  VALUES ('Darth Vade', 'black-attire@starwars.io')
+  VALUES ('Darth Vader', 'black-attire@starwars.io')
   RETURNING id INTO l_todo_user_id;
   
   INSERT INTO todo (title, description, deadline, notifications, user_id)
@@ -68,6 +68,40 @@ BEGIN
   
   INSERT INTO todo_comment (body, user_id, todo_id)
   VALUES ('I must obey my master', l_todo_user_id, l_todo_id);
+
+  -- 3. Data for Princess Leia
+  INSERT INTO todo_user (name, email)
+  VALUES ('General Leia', 'princess@starwars.io')
+  RETURNING id INTO l_todo_user_id;
+  
+  INSERT INTO todo (title, description, deadline, notifications, user_id)
+  VALUES ('Return of the Jedi', 'I feel very passionate about Luke', SYSTIMESTAMP - INTERVAL '60' DAY, SYSTIMESTAMP - INTERVAL '60' DAY, l_todo_user_id)
+  RETURNING id INTO l_todo_id;
+  
+  INSERT INTO category (name, priority, color)
+  VALUES ('Jedi Apprentice', 10, 'green')
+  RETURNING id INTO l_category_id;
+  
+  INSERT INTO todo_category (is_planned, category_id, todo_id)
+  VALUES ('T', l_category_id, l_todo_id);
+  
+  INSERT INTO todo_comment (body, user_id, todo_id)
+  VALUES ('My father was seduced by the dark side', l_todo_user_id, l_todo_id);
+  
+  INSERT INTO todo (title, description, deadline, notifications, user_id)
+  VALUES ('Someone has to save our skins', 'Punishing Han Solo', SYSTIMESTAMP - INTERVAL '60' DAY, SYSTIMESTAMP - INTERVAL '60' DAY, l_todo_user_id)
+  RETURNING id INTO l_todo_id;
+  
+  INSERT INTO category (name, priority, color)
+  VALUES ('Jedi Master', 3, 'blue')
+  RETURNING id INTO l_category_id;
+  
+  INSERT INTO todo_category (is_planned, category_id, todo_id)
+  VALUES ('F', l_category_id, l_todo_id);
+  
+  INSERT INTO todo_comment (body, user_id, todo_id)
+  VALUES ('Punish Kylo Ren', l_todo_user_id, l_todo_id);
+
 
   COMMIT;
 END;
